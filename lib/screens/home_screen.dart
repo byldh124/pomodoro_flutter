d2130f7f6812b0dc017945a265ab860e51af172f
byldh <byldh124@gmail.com>
2023-11-13 14:39:46 +0900

    border radius

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -134,7 +134,9 @@ class _HomeScreenState extends State<HomeScreen> {
                     child: Container(
                       decoration: BoxDecoration(
                           color: Theme.of(context).cardColor,
-                          borderRadius: BorderRadius.circular(50)),
+                          borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(50),
+                              topRight: Radius.circular(50))),
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
